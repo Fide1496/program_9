2f59ce18debbe86b867134cff6fbc43bb21000af
faith <faith@toyridge.com>
2025-03-26 06:46:11 -0500

    blah

--- main.c
+++ main.c
@@ -14,6 +14,8 @@ int guess[2] = {0, 0};
 int cmp[2] = {0, 0};
 int rdy[4] = {0, 0, 0, 0};
 
+volatile int paus = 1;
+
 pthread_mutex_t mtx[3];
 pthread_cond_t cnd[3];
 
@@ -26,19 +28,33 @@ int checkError(int val, const char *msg) {
     return val;
 }
 
-void *player_one(void *arg){
-    int min = 0;
-    int max = 0;
+// Check thread error
+int checkTError(int val, const char *msg) {
+    if (val == -1) {
+        perror(msg);
+        exit(EXIT_FAILURE);
+    }
+    return 0;
+}
 
-    while(1){
+void *player_one(void * args){
+    int min = 0;
+    int max = 100;
+
+    while(paus){
         pthread_mutex_lock(&mtx[2]);
         while(rdy[2]==0){
             pthread_cond_wait(&cnd[2],&mtx[2]);
         }
         pthread_mutex_unlock(&mtx[2]);
         rdy[2]=0;
+
+        int min = 0;
+        int max = 100;
+        
         while(1){
             guess[0] = (min+max) /2;
+            printf("Player 1 guesses: %d\n", guess[0]);
 
             pthread_mutex_lock(&mtx[0]);
             rdy[0] = 1;
@@ -60,11 +76,12 @@ void *player_one(void *arg){
     return NULL;
 
 }
-void *player_two(void *arg) {
-    int min = 0, max = 100;
+void *player_two(void * args) {
+    int min = 0;
+    int max = 100;
     srand(time(NULL));
 
-    while (1) {
+    while (paus) {
         pthread_mutex_lock(&mtx[2]);
         while (rdy[3] == 0) {
             pthread_cond_wait(&cnd[2], &mtx[2]);
@@ -72,8 +89,12 @@ void *player_two(void *arg) {
         pthread_mutex_unlock(&mtx[2]);
         rdy[3] = 0;
 
+        int min = 0;
+        int max = 100;
+
         while (1) {
             guess[1] = min + rand() % (max - min + 1);
+            printf("Player 2 guesses: %d\n", guess[1]);
 
             pthread_mutex_lock(&mtx[1]);
             rdy[1] = 1;
@@ -94,8 +115,9 @@ void *player_two(void *arg) {
     return NULL;
 }
 
-void *referee(void *arg) {
-    for (int game = 1; game <= 10; game++) {
+void *referee(void * arg) {
+    int game_num =10;
+    for (game_num = 1; game_num <= 10; game_num++) {
         int target = 1 + rand() % 100;
 
         pthread_mutex_lock(&mtx[2]);
@@ -104,7 +126,7 @@ void *referee(void *arg) {
         pthread_cond_broadcast(&cnd[2]);
         pthread_mutex_unlock(&mtx[2]);
 
-        printf("Game %d:\n", game);
+        printf("Game %d:\n", game_num);
 
         while (1) {
             sleep(1);
@@ -118,16 +140,19 @@ void *referee(void *arg) {
             cmp[0] = guess[0] - target;
             cmp[1] = guess[1] - target;
 
+            rdy[0] = 1;
+            rdy[1] = 1;
+
             if (cmp[0] == 0 || cmp[1] == 0) {
                 cmp[0] = cmp[1] = 0;
-                printf("Game %d: A player guessed the correct number!\n", game);
+                printf("Game %d: A player guessed the correct number!\n", game_num);
                 pthread_mutex_unlock(&mtx[1]);
                 pthread_mutex_unlock(&mtx[0]);
                 break;
             }
 
-            rdy[0] = 1;
-            rdy[1] = 1;
+            // rdy[0] = 1;
+            // rdy[1] = 1;
             pthread_cond_signal(&cnd[0]);
             pthread_cond_signal(&cnd[1]);
 
@@ -135,27 +160,29 @@ void *referee(void *arg) {
             pthread_mutex_unlock(&mtx[0]);
         }
     }
+    paus = 0;
+    pthread_cond_broadcast(&cnd[0]);
+    pthread_cond_broadcast(&cnd[1]);
+    pthread_cond_broadcast(&cnd[2]);
     return NULL;
 }
 
 
-int main(){
+int main(int argc, char *argv[]){
 
     srand(time(NULL));
+    pthread_t thrds[3];
+    void *(*fcns[3])(void *) = {player_one, player_two, referee};
+    int i, j;
 
-    for (int i =0; i < 3; i++){
 
-        pthread_mutex_init(&mtx[i], NULL);
-        pthread_mutex_init(&mtx[i], NULL);
+    for (i = 0; i < 3; i++) {
+        pthread_create(&thrds[i], NULL,fcns[i], NULL);
     }
 
-    pthread_t p1,p2, ref;
-
-    pthread_create(&p1, NULL,player_one, NULL);
-    pthread_create(&p2,NULL,player_two,NULL);
-    pthread_create(&ref,NULL,referee,NULL);
-
-    pthread_join(ref,NULL);
+    for (j = 0; j < 3; j++) {
+        pthread_join(thrds[j], NULL);
+    }
 
     exit(EXIT_SUCCESS);
 }
